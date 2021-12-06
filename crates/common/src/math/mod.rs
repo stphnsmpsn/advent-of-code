fd6e0a09f5ae46d702ef664cac0f8753adb7d4d6
Steve Sampson <mail@stephensampson.dev>
2021-12-06 17:01:06 -0400

    Simplify MyCheckedOps Impl for u32

--- crates/common/src/math/mod.rs
+++ crates/common/src/math/mod.rs
@@ -6,49 +6,43 @@ pub trait MyCheckedOps {
     fn my_checked_mul(&mut self, rhs: u32) -> Result<(), AocError>;
 }
 
+enum Operation {
+    Add,
+    Subtract,
+    Multiply,
+}
+
 impl MyCheckedOps for u32 {
     fn my_checked_add(&mut self, rhs: u32) -> Result<(), AocError> {
-        match self.checked_add(rhs) {
-            Some(res) => {
-                *self = res;
-                Ok(())
-            }
-            None => {
-                return Err(AocError::OverflowError(format!(
-                    "Overflow calculating: {} + {}",
-                    self, rhs
-                )))
-            }
-        }
+        do_work(self, rhs, Operation::Add)
     }
 
     fn my_checked_sub(&mut self, rhs: u32) -> Result<(), AocError> {
-        match self.checked_sub(rhs) {
-            Some(res) => {
-                *self = res;
-                Ok(())
-            }
-            None => {
-                return Err(AocError::OverflowError(format!(
-                    "Overflow calculating: {} - {}",
-                    self, rhs
-                )))
-            }
-        }
+        do_work(self, rhs, Operation::Subtract)
     }
 
     fn my_checked_mul(&mut self, rhs: u32) -> Result<(), AocError> {
-        match self.checked_mul(rhs) {
-            Some(res) => {
-                *self = res;
-                Ok(())
-            }
-            None => {
-                return Err(AocError::OverflowError(format!(
-                    "Overflow calculating: {} * {}",
-                    self, rhs
-                )))
-            }
+        do_work(self, rhs, Operation::Multiply)
+    }
+}
+
+fn do_work(me: &mut u32, rhs: u32, op: Operation) -> Result<(), AocError> {
+    let res = match op {
+        Operation::Add => me.checked_add(rhs),
+        Operation::Subtract => me.checked_sub(rhs),
+        Operation::Multiply => me.checked_mul(rhs),
+    };
+
+    match res {
+        Some(res) => {
+            *me = res;
+            Ok(())
+        }
+        None => {
+            return Err(AocError::OverflowError(format!(
+                "Overflow calculating: {} * {}",
+                me, rhs
+            )))
         }
     }
 }
